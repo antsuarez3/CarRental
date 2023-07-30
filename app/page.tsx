0c4875adeb81b2956d19671616bf722d1bab89fc
anthonysuarez <antsuarez305@gmail.com>
2023-07-30 17:36:21 -0500

    Update custom filter buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants'
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default async function Home({searchParams}) {
+export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '',
     year: searchParams.year || 2022,
@@ -27,8 +28,8 @@ export default async function Home({searchParams}) {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
