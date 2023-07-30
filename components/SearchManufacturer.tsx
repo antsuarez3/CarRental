07c61ea45b875826e3ec8a97e9df08bd7edb363b
anthonysuarez <antsuarez305@gmail.com>
2023-07-30 13:50:44 -0500

    Update search bar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useState, Fragment } from 'react'
 import Image from 'next/image'
+import { useState, Fragment } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 
 import { manufacturers } from '@/constants'
