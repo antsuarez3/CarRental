f9f87526707135fdcd8232759ed9895fccdf4259
anthonysuarez <antsuarez305@gmail.com>
2023-08-03 16:53:53 -0500

    Update ShowMore page

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { Fragment, useState } from 'react'
+import { Fragment, useState } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { Listbox, Transition } from '@headlessui/react'
